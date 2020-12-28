20587df96b764f14456a0ba0e82ec64fcd515629
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-28 15:49:31 +0600

    MyController updated

--- src/main/java/com/kaikenov/spring/mvc/MyController.java
+++ src/main/java/com/kaikenov/spring/mvc/MyController.java
@@ -2,10 +2,8 @@ package com.kaikenov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/employee")
@@ -17,25 +15,16 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails() {
+    public String askEmployeeDetails(Model model) {
+        model.addAttribute("employee", new Employee());
         return "ask-emp-details-view";
     }
 
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr/Mrs " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//
-//        return "show-emp-details-view";
-//    }
-
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
+    public String showEmployeeDetails(@ModelAttribute("employee") Employee employee) {
 
-        empName = "Mr/Mrs " + empName;
-        model.addAttribute("nameAttribute", empName);
+        String employeeName = employee.getName();
+        employee.setName("Mr/Mrs" + employeeName);
 
         return "show-emp-details-view";
     }
