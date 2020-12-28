5857eb8dcd0b5e8dd9f4442c26fa044fd74512c1
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-28 19:12:39 +0600

    Added java bean validation

--- src/main/java/com/kaikenov/spring/mvc/Employee.java
+++ src/main/java/com/kaikenov/spring/mvc/Employee.java
@@ -1,12 +1,17 @@
 package com.kaikenov.spring.mvc;
 
+import javax.validation.constraints.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Employee {
 
+    @Size(min = 2, message = "Name must be minimum 2 symbols")
     private String name;
+    @NotBlank(message = "Surname is required field")
     private String surname;
+    @Min(value = 500, message = "must be greater 499")
+    @Max(value = 1000, message = "must be less than 1001")
     private String salary;
     private String department;
     private Map<String, String> departments;
@@ -14,6 +19,8 @@ public class Employee {
     private Map<String, String> carBrands;
     private String[] languages;
     private Map<String, String> languageList;
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{2}", message = "please use pattern XXX-XX-XX")
+    private String phoneNumber;
 
     public Employee() {
         this.departments = new HashMap<>();
@@ -109,6 +116,14 @@ public class Employee {
 
     public void setLanguageList(Map<String, String> languageList) {
         this.languageList = languageList;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     @Override
--- src/main/java/com/kaikenov/spring/mvc/MyController.java
+++ src/main/java/com/kaikenov/spring/mvc/MyController.java
@@ -2,8 +2,11 @@ package com.kaikenov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -21,7 +24,12 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(@ModelAttribute("employee") Employee employee) {
+    public String showEmployeeDetails(@Valid @ModelAttribute("employee") Employee employee,
+                                      BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        }
 
         String employeeName = employee.getName();
         employee.setName("Mr/Mrs" + employeeName);
